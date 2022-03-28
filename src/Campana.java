b0d6c9360be3270d9a742dae1da2c6a3d0a659ad
lai2-01 <lai2-01@itismeucci.com>
2022-03-28 13:48:21 +0200

    din don dan

--- src/Campana.java
+++ src/Campana.java
@@ -9,7 +9,9 @@ public class Campana implements Runnable {
     }
     public void run(){
         for(int i=0;i<volte;i++){
-            System.out.println((i+1)+"suono"+" ");
+            System.out.println((i+1)+suono+" ");
+            
         }
+        
     }
 }
--- src/Dindonda.java
+++ src/Dindonda.java
@@ -2,9 +2,36 @@ import java.lang.Runnable;
 
 public class Dindonda {
 public static void main(String[] args) {
+for(int r=0;r<3;r++){
+Thread i=new Thread(new Campana("din", 5));
 
-new Thread(new Campana("din", 5)).start();
-new Thread(new Campana("don",5)).start();
+i.start();
+try {
+    i.sleep(0);
+} catch (Exception e) {
+    System.out.println(e.getMessage());
+}
 
+
+Thread y=new Thread(new Campana("don",5));
+
+y.start();
+try {
+    y.sleep(500);
+} catch (Exception e) {
+    System.out.println(e.getMessage());
+}
+
+Thread t=new Thread(new Campana("dan",5));
+
+t.start();
+try {
+    t.sleep(1000);
+} catch (Exception e) {
+
+    System.out.println(e.getMessage());
+}
+
+}
 }
 }
